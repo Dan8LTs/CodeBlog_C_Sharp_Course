85d3031a88337bfcc595df059c76f2cbf58333d8
LTs <LTs@DESKTOP-E7MNNM8>
2025-05-20 01:42:14 +0300

    Commit #11

---
+++ Parameters/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Parameters
+{
+    class Program
+    {
+        static int value = 0;
+        static void Main(string[] args)
+        {
+            Method();
+            Method(6, "B");
+            Method(2, g: Guid.NewGuid(), dt: DateTime.Now);
+            Method(value++, value++.ToString());
+            Console.WriteLine(value);
+            Method(s: (value++).ToString(), x: value++);
+            Console.WriteLine(value);
+        }
+        static void Method(int x = 8, string s = "S", DateTime dt = default, Guid g = new Guid())
+        {
+            Console.WriteLine($"x = {x}, s = {s}, dt = {dt}, guid = {g}");
+        }
+    }
+}
---
+++ Var/Program.cs
@@ -0,0 +1,30 @@
+﻿// var использует тип данных после =
+// должен быть сразу инициализирован при объявлении
+var name = "Dan8LTs";
+ShowVariableType(name);
+
+// Exception x = null;
+var x = (Exception)null;
+ShowVariableType(x);
+
+var numbers = new int[] { 3, 2, 23, 5 };
+ShowVariableType(numbers);
+
+var collection = new Dictionary<string, double>() { { "age", 18 } };
+ShowVariableType(collection);
+
+foreach (var item in collection)
+{
+    ShowVariableType(item);
+}
+
+var i = M();
+static int M()
+{
+    return 0;
+}
+
+static void ShowVariableType<T>(T variable)
+{
+    Console.WriteLine(typeof(T));
+}
