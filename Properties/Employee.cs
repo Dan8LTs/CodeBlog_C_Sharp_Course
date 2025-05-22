a23d843a925a854bbbd44bfa391ff57d561c5b15
LTs <LTs@DESKTOP-E7MNNM8>
2025-05-22 23:58:15 +0300

    Commit #13

---
+++ Properties/Employee.cs
@@ -0,0 +1,55 @@
+﻿namespace Properties
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        // автосвойства
+        public string Profession { get; set; }
+
+        // get и set - методы аксессоры для доступа к приватным полям
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                this.name = name;
+            }
+            else
+            {
+                throw new ArgumentNullException("name is empty");
+            }
+        }
+
+        // упрощённое создание аксессоров
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("age is incorrect");
+                }
+            }
+        }
+        public int YearOfBirth
+        {
+            get
+            {
+                return DateTime.Now.Year - age;
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Properties
+{
+    internal class Program()
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            p1.SetName("Vlad");
+            Console.WriteLine(p1.GetName());
+
+            // обращение не к полю, а к методу - аксессору
+            p1.Age = 19;
+            Console.WriteLine(p1.Age);
+            Console.WriteLine(p1.YearOfBirth);
+
+            p1.Profession = "sportsman";
+            Console.WriteLine(p1.Profession);
+        }
+    }
+}
